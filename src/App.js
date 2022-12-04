74444ae4c65ca3aeebaa8cc95a7c262aed899363
dzordich <dzordich@gmail.com>
2022-12-03 20:03:24 -0500

    add likedshowsstate to app.js; add heart icon to liked tracks; finally commit theme updates

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { NavigationContainer } from "@react-navigation/native";
-import Routes from "./features/Routes";
-import { Layout, ViewPort } from "./ui/Layout";
-import CityProvider from "./features/cities/CityProvider";
-import PlayerState from "./features/player/PlayerState";
-import Player, { PlayerSafeArea } from "./features/player/Player";
-import { navigationRef } from "./utils/navigation";
-import BannerPlaylistProvider from "./features/playlists/BannerPlaylistProvider";
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './features/Routes';
+import { Layout, ViewPort } from './ui/Layout';
+import CityProvider from './features/cities/CityProvider';
+import PlayerState from './features/player/PlayerState';
+import Player, { PlayerSafeArea } from './features/player/Player';
+import { navigationRef } from './utils/navigation';
+import BannerPlaylistProvider from './features/playlists/BannerPlaylistProvider';
+import LikedShowsState from './utils/hooks/LikedShowsState';
 
 export default function App() {
-  return (
-    <NavigationContainer ref={navigationRef}>
-      <SafeAreaProvider>
-        <Layout>
-          <ViewPort>
-            <StatusBar style="light" />
-            <CityProvider>
-              <BannerPlaylistProvider>
-                <PlayerState>
-                  <PlayerSafeArea>
-                    <Routes />
-                  </PlayerSafeArea>
-                  <Player />
-                </PlayerState>
-              </BannerPlaylistProvider>
-            </CityProvider>
-          </ViewPort>
-        </Layout>
-      </SafeAreaProvider>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer ref={navigationRef}>
+            <SafeAreaProvider>
+                <Layout>
+                    <ViewPort>
+                        <StatusBar style="light" />
+                        <CityProvider>
+                            <BannerPlaylistProvider>
+                                <LikedShowsState>
+                                    <PlayerState>
+                                        <PlayerSafeArea>
+                                            <Routes />
+                                        </PlayerSafeArea>
+                                        <Player />
+                                    </PlayerState>
+                                </LikedShowsState>
+                            </BannerPlaylistProvider>
+                        </CityProvider>
+                    </ViewPort>
+                </Layout>
+            </SafeAreaProvider>
+        </NavigationContainer>
+    );
 }
--- src/features/playlists/Track.js
+++ src/features/playlists/Track.js
@@ -1,87 +1,102 @@
-import React from "react";
-import styled from "styled-components/native";
-import { TouchableHighlight, View } from "react-native";
-import { any } from "ramda";
-import { H3, Text } from "../../ui/Text";
-import { colors } from "../../ui/theme";
-import AlbumArt from "./AlbumArt";
-import ShiftRight from "../../ui/layout/ShiftRight";
-import IconButton from "../../ui/inputs/IconButton";
-import InfoIcon from "../../ui/icons/InfoIcon";
-import { navigate } from "../../utils/navigation";
-import PATHS from "../../contants/paths";
-import { FreeMarker, FestivalMarker } from "../../ui/Marker";
+import React from 'react';
+import styled from 'styled-components/native';
+import { TouchableHighlight, View } from 'react-native';
+import { any } from 'ramda';
+import { H3, Text } from '../../ui/Text';
+import { colors } from '../../ui/theme';
+import AlbumArt from './AlbumArt';
+import ShiftRight from '../../ui/layout/ShiftRight';
+import IconButton from '../../ui/inputs/IconButton';
+import InfoIcon from '../../ui/icons/InfoIcon';
+import { navigate } from '../../utils/navigation';
+import PATHS from '../../contants/paths';
+import { FreeMarker, FestivalMarker } from '../../ui/Marker';
+import { useLikedShows } from '../../utils/hooks/LikedShowsState';
+import FilledInHeartIcon from '../../ui/icons/FilledInHeartIcon';
 
 const TrackContainer = styled(View)`
-  flex: 1;
-  flex-direction: row;
-  align-items: center;
-  width: 100%;
-  margin: 8px;
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+    margin: 8px;
+    z-index: -1;
 `;
 
 const TrackTitle = styled(H3)`
-  margin-top: 4px;
-  ${(props) => (props.currentTrack ? `color: ${colors.secondary70};` : "")}
+    margin-top: 4px;
+    ${props => (props.currentTrack ? `color: ${colors.secondary70};` : '')}
 `;
 
 const TrackText = styled(View)`
-  margin-left: 16px;
-  flex: 1;
+    margin-left: 16px;
+    flex: 1;
 `;
 
 const TrackSubText = styled(Text)`
-  color: ${colors.neutral80};
-  font-size: 13px;
+    color: ${colors.neutral80};
+    font-size: 12px;
 `;
 
-const hasFreeShow = any((show) => !!show.free);
+const hasFreeShow = any(show => !!show.free);
 
 const Track = ({ performer, onPress, currentTrack, trackIndex }) => {
-  const { top_track: track, name, shows } = performer;
+    const { top_track: track, name, shows } = performer;
+    const { isShowLiked } = useLikedShows();
 
-  return (
-    <TouchableHighlight
-      onPress={async () => onPress(track, trackIndex)}
-      activeOpacity={1}
-      underlayColor={colors.neutral5}
-    >
-      <TrackContainer>
-        <AlbumArt url={track?.album_art_url} />
-        <TrackText>
-          <TrackSubText numberOfLines={1}>{track.name}</TrackSubText>
+    return (
+        <TouchableHighlight
+            onPress={async () => onPress(track, trackIndex)}
+            activeOpacity={1}
+            underlayColor={colors.neutral5}
+        >
+            <TrackContainer>
+                <AlbumArt url={track?.album_art_url} />
+                <TrackText>
+                    <TrackSubText numberOfLines={1}>{track.name}</TrackSubText>
 
-          <TrackTitle
-            currentTrack={track && track.id === currentTrack?.id}
-            numberOfLines={1}
-          >
-            {name}
-            <TrackSubText numberOfLines={1}>
-              {shows.length > 0 ? ` — ${shows[0].venue.name}` : ""}
-            </TrackSubText>
-          </TrackTitle>
-        </TrackText>
-        <ShiftRight>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              justifyContent: "flex-end",
-              alignItems: "center",
-              paddingLeft: 4,
-            }}
-          >
-            <FestivalMarker shows={shows} />
-            <FreeMarker free={hasFreeShow(shows)} />
-            <IconButton
-              Icon={InfoIcon}
-              onPress={() => navigate(PATHS.SHOW_DETAILS, performer)}
-            />
-          </View>
-        </ShiftRight>
-      </TrackContainer>
-    </TouchableHighlight>
-  );
+                    <TrackTitle
+                        currentTrack={track && track.id === currentTrack?.id}
+                        numberOfLines={1}
+                    >
+                        {name}
+                        <TrackSubText numberOfLines={1}>
+                            {shows.length > 0
+                                ? ` — ${shows[0].venue.name}`
+                                : ''}
+                        </TrackSubText>
+                    </TrackTitle>
+                </TrackText>
+                <ShiftRight>
+                    <View
+                        style={{
+                            flex: 1,
+                            flexDirection: 'row',
+                            justifyContent: 'flex-end',
+                            alignItems: 'center',
+                            paddingLeft: 4,
+                        }}
+                    >
+                        <FestivalMarker shows={shows} />
+                        <FreeMarker free={hasFreeShow(shows)} />
+                        {shows.some(({ id }) => isShowLiked(id)) && (
+                            <FilledInHeartIcon
+                                width="14px"
+                                height="14px"
+                                style={{ marginHorizontal: 8 }}
+                            />
+                        )}
+                        <IconButton
+                            Icon={InfoIcon}
+                            onPress={() =>
+                                navigate(PATHS.SHOW_DETAILS, performer)
+                            }
+                        />
+                    </View>
+                </ShiftRight>
+            </TrackContainer>
+        </TouchableHighlight>
+    );
 };
 
 export default Track;
